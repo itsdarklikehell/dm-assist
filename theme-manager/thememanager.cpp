e9fc021b47d78465971b68b85e97927f5e901e92
Technohamster <arseniy.khomchenko@gmail.com>
2025-09-04 09:32:52 +0300

    Изменены цвета темной темы

--- theme-manager/thememanager.cpp
+++ theme-manager/thememanager.cpp
@@ -30,15 +30,16 @@ void ThemeManager::applyPreset(ThemeManager::PresetTheme theme) {
             palette.setColor(QPalette::HighlightedText, Qt::white);
             break;
         case PresetTheme::Dark:
-            palette.setColor(QPalette::Window, QColor("#121212"));
+            palette.setColor(QPalette::Window, QColor("#1e1f22"));
             palette.setColor(QPalette::WindowText, Qt::white);
-            palette.setColor(QPalette::Base, QColor("#1e1e1e"));
+            palette.setColor(QPalette::Base, QColor("#2b2d30"));
             palette.setColor(QPalette::Text, QColor("#e0e0e0"));
             palette.setColor(QPalette::Button, QColor("#2d2d2d"));
             palette.setColor(QPalette::ButtonText, Qt::white);
             palette.setColor(QPalette::Highlight, QColor("#3f51b5"));
             palette.setColor(QPalette::HighlightedText, Qt::white);
             palette.setColor(QPalette::Link, QColor("#55aaff"));
+            palette.setColor(QPalette::PlaceholderText, QColor("#efefef"));
             break;
         default:
             resetToSystemTheme();
